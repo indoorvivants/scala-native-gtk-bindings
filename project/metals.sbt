e744b9d5af3b87595f236ea6980bf5ada9b28e9a
Anton Sviridov <keynmol@gmail.com>
2023-02-09 10:39:29 +0000

    Beef up example

--- project/metals.sbt
+++ project/metals.sbt
@@ -3,7 +3,7 @@
 // This plugin enables semantic information to be produced by sbt.
 // It also adds support for debugging using the Debug Adapter Protocol
 resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-addSbtPlugin("org.scalameta" % "sbt-metals" % "0.11.10+117-476976c1-SNAPSHOT")
+addSbtPlugin("org.scalameta" % "sbt-metals" % "0.11.10+121-88866fbb-SNAPSHOT")
 
 // This plugin adds the BSP debug capability to sbt server.
 
--- src/main/scala/example.scala
+++ src/main/scala/example.scala
@@ -4,6 +4,7 @@ import gtk.functions.*
 import gtk.enumerations.*
 import gtk.structs.*
 import gtk.aliases.*
+import gtk.fluent.*
 
 import scalanative.unsafe.*
 
@@ -19,31 +20,38 @@ import scalanative.unsafe.*
         window.asInstanceOf[Ptr[GtkWindow]],
         c"Hello from Scala Native"
       )
+      gtk_window_set_default_size(window.asPtr[GtkWindow], 200, 200)
 
-      gtk_window_set_default_size(window.asInstanceOf[Ptr[GtkWindow]], 500, 500)
+      val box = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)
+      gtk_widget_set_halign(box, GtkAlign.GTK_ALIGN_CENTER)
+      gtk_widget_set_valign(box, GtkAlign.GTK_ALIGN_CENTER)
+
+      gtk_window_set_child(window.asPtr[GtkWindow], box)
+
+      val button = gtk_button_new_with_label(c"Press me")
+
+      val printHello = CFuncPtr2.fromScalaFunction {
+        (widget: Ptr[GtkWidget], data: gpointer) =>
+          g_print(c"Yoooo, click!".asGString)
+      }
+
+      g_signal_connect(button, c"clicked", printHello)
+
+      gtk_box_append(box.asPtr[GtkBox], button)
 
       gtk_widget_show(window)
   }
-
-  val g_callback = GCallback(
-    CFuncPtr.fromPtr[CFuncPtr0[Unit]](CFuncPtr.toPtr(callback))
-  )
 
   val app = gtk_application_new(
     c"org.gtk.example",
     GApplicationFlags.G_APPLICATION_FLAGS_NONE
   )
 
-  g_signal_connect_data(
-    app.asInstanceOf[gpointer],
-    c"activate".asInstanceOf[Ptr[gchar]],
-    g_callback,
-    null.asInstanceOf[gpointer],
-    null.asInstanceOf[GClosureNotify],
-    GConnectFlags.define(0L)
+  g_signal_connect(
+    app,
+    c"activate",
+    callback
   )
 
-  g_application_run(app.asInstanceOf[Ptr[GApplication]], 0, null)
-
-  System.in.read()
+  g_application_run(app.asPtr[GApplication], 0, null)
 end example
