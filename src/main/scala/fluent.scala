497139b755313d4a5d7a8282493db280f06adedd
Anton Sviridov <keynmol@gmail.com>
2023-02-09 10:47:31 +0000

    fluency level

---
+++ src/main/scala/fluent.scala
@@ -0,0 +1,37 @@
+package gtk
+package fluent
+
+import scalanative.unsafe.*
+
+import gtk.aliases.*
+import gtk.functions.g_signal_connect_data
+import gtk.enumerations.GConnectFlags
+
+extension [T <: CFuncPtr](inline ptr: T)
+  inline def asGCallback: GCallback =
+    GCallback(
+      CFuncPtr.fromPtr[CFuncPtr0[Unit]](CFuncPtr.toPtr(ptr))
+    )
+
+extension (cstr: CString)
+  inline def asGString: Ptr[gchar] = cstr.asInstanceOf[Ptr[gchar]]
+
+extension [T](ptr: Ptr[T])
+  inline def asGpointer: gpointer = gpointer(ptr.asInstanceOf[Ptr[Byte]])
+  inline def asPtr[T1]: Ptr[T1] = ptr.asInstanceOf[Ptr[T1]]
+
+inline def g_signal_connect[A: Tag, T <: CFuncPtr](
+    instance: Ptr[A],
+    detailed_signal: CString,
+    c_handler: T,
+    data: Ptr[Byte] = null,
+    flags: GConnectFlags = GConnectFlags.define(0L)
+): gulong =
+  g_signal_connect_data(
+    instance.asGpointer,
+    detailed_signal.asGString,
+    c_handler.asGCallback,
+    data.asGpointer,
+    null.asInstanceOf[GClosureNotify],
+    flags
+  )
