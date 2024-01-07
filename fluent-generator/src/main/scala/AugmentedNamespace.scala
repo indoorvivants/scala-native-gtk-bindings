82b186a99ba38925901881f185c40b7f31efd222
Anton Sviridov <keynmol@gmail.com>
2024-01-07 13:53:10 +0000

    WIP

--- fluent-generator/src/main/scala/AugmentedNamespace.scala
+++ fluent-generator/src/main/scala/AugmentedNamespace.scala
@@ -6,6 +6,7 @@ import java.nio.file.Path
 import java.nio.file.Files
 import scala.jdk.CollectionConverters.*
 import scala.reflect.ClassTag
+import scalaxb.DataRecord
 
 case class AugmentedNamespace(n: Namespace):
   export n.*
@@ -15,9 +16,40 @@ case class AugmentedNamespace(n: Namespace):
     n.namespaceoption.collect:
       case d if ct.unapply(d.value).isDefined => d.as[T]
 
-  lazy val interfaces: Seq[Interface] =
-    collect[Interface]
+  lazy val interfaces: Seq[AugmentedInterface] =
+    collect[Interface].map(AugmentedInterface(_))
 
-  lazy val classes: Seq[Class] =
-    collect[Class]
+  lazy val classes: Seq[AugmentedClass] =
+    collect[Class].map(AugmentedClass(_))
 end AugmentedNamespace
+
+trait ClassLike:
+  protected def options: Seq[DataRecord[Any]]
+  def collect[T: scala.reflect.ClassTag] =
+    val ct = summon[ClassTag[T]]
+    options.collect:
+      case d if ct.unapply(d.value).isDefined => d.as[T]
+
+  lazy val implements: Seq[Implements] =
+    collect[Implements]
+
+  lazy val methods: Seq[Method] =
+    collect[Method]
+
+  lazy val constructors: Seq[Constructor] =
+    collect[Constructor]
+end ClassLike
+
+case class AugmentedInterface(n: Interface) extends ClassLike:
+  export n.*
+
+  override protected def options: Seq[DataRecord[Any]] = n.interfaceoption
+case class AugmentedClass(n: Class) extends ClassLike:
+  export n.*
+  override protected def options: Seq[DataRecord[Any]] = n.classoption
+
+end AugmentedClass
+
+extension (c: Constructor)
+  def parameters = c.constructoroption.collect:
+    case d if d.value.isInstanceOf[Parameter] => d.as[Parameter]
--- fluent-generator/src/main/scala/Generate.scala
+++ fluent-generator/src/main/scala/Generate.scala
@@ -6,20 +6,19 @@ import java.nio.file.Path
 import java.nio.file.Files
 import scala.jdk.CollectionConverters.*
 import rendition.*
+import scala.util.boundary, boundary.break
 
 @main def hello(girFilesPath: String) =
   val root = Paths.get(girFilesPath).toAbsolutePath()
   val includeResolver = IncludeResolver(root)
   val reader = Reader(includeResolver)
-  val newValue = reader("gio-2.0").namespace.get
+  val newValue = reader("gdkpixbuf-2.0").namespace.get
 
   val lb = LineBuilder()
 
   lb.render { r =>
     import r.*
-
     renderNamespace(r, newValue)
-
   }
 
   println(lb.result)
@@ -29,5 +28,48 @@ end hello
 def renderNamespace(r: Rendering, ns: AugmentedNamespace) =
   r.use:
     import r.*
+
+    def extensions(parent: Option[String], impl: Seq[Implements]) =
+      val ext = parent.toSeq ++ impl.map(_.name)
+      if ext.nonEmpty then " extends " + ext.mkString(", ")
+      else ""
+
     ns.interfaces.map: iface =>
-      line(s"trait ${iface.name}")
+      line(s"trait ${iface.name}${extensions(None, iface.implements)}")
+
+    ns.classes.map: cls =>
+      val skippedBecause =
+        boundary:
+          val cTypeName = cls.attributes.get("@type").map(_.as[String])
+          val cType = qualifyCType(cTypeName.getOrElse(break("c:type missing")))
+          val data = s"(raw: Ptr[$cType])"
+          block(
+            s"class ${cls.name}$data${extensions(cls.parent, cls.implements)}:",
+            s"end ${cls.name}"
+          ):
+            cls.methods.foreach: meth =>
+              val camelName = camelify(meth.name)
+              val cMethod = meth.identifier
+              if meth.name == "get_height" then println(meth.methodoption)
+              line(s"def $camelName = ??? /*$cMethod*/")
+
+          block(s"object ${cls.name}", s"end ${cls.name}"):
+            cls.constructors.foreach: constructor =>
+              val cConstructor = constructor.identifier
+              val sanitisedName = constructor.name match
+                case "new" => "apply"
+                case s"new_$rest" =>
+                  camelify(rest)
+              line(s"def $sanitisedName(): ${cls.name} = ??? /*$cConstructor*/")
+
+      skippedBecause match
+        case msg: String =>
+          scribe.warn(s"${cls.name} is broken: `$msg`")
+        case _ =>
+
+def camelify(name: String) =
+  val els = name.split("_").map(_.capitalize)
+  els(0) = els(0).toLowerCase()
+  els.mkString
+
+def qualifyCType(name: String) = s"???.$name"
